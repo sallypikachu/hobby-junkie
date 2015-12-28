426a57842f7953c9136d5e567a0836cf9ad5a42a
sallypikachu <sallypikachu@gmail.com>
2015-12-27 23:29:49 -0500

    Creating omniauth spec

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -2,10 +2,13 @@ class HobbiesController < ApplicationController
   def index
     @hobbies = Hobby.all
     @user = User.find_by(id: session[:user_id])
-    binding.pry
   end
 
   def show
     @hobby = Hobby.find(params[:id])
+  end
+
+  def new
+    @hobby = Hobby.new
   end
 end
