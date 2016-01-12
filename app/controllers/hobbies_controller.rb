7ce160a431143d2d4a89f6d1f7fcd36d521681a5
sallypikachu <sallypikachu@gmail.com>
2016-01-11 21:04:14 -0500

    embed video using Youtube API

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,4 +1,7 @@
 class HobbiesController < ApplicationController
+  before_action :correct_user,   only: [:index, :show]
+  before_action :logged_in_user, only: [:index, :show]
+
   def index
     @user = User.find_by(id: session[:user_id])
     @hobbies = Hobby.where(user_id: session[:user_id])
@@ -31,5 +34,18 @@ class HobbiesController < ApplicationController
     :name,
     :description
     )
+  end
+
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by(id: session[:user_id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
